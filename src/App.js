b641990c34b53b7cbde3bb790e4bff44699c915e
unknown <brendan.coutts88@gmail.com>
2022-06-05 14:12:22 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+const [movie, setMovie] = useState({})
+
+ useEffect(()=> {
+ async function getMovies() {
+   try {
+    const res = await fetch('https://api.themoviedb.org/3/movie/76341?api_key=31dab1ca58484568ac09b8a656e2dd73')
+    const data = await res.json()
+    setMovie(data)
+   } catch (error) {
+     console.log(error)
+   }
+ }
+getMovies()
+
+
+
+ }, [])
+
+console.log(movie.title)
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{movie.title}</h2>
     </div>
   );
 }
